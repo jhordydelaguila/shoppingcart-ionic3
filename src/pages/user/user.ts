b8f2dcef6b9d4db35a4be0d9bd3c22e01308a410
Jhordy Alexi <jhordydelaguila@hotmail.com>
2019-06-20 00:27:09 -0500

    Add(User): menu for user

---
+++ src/pages/user/user.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-user',
+  templateUrl: 'user.html',
+})
+export class UserPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad UserPage');
+  }
+
+}
